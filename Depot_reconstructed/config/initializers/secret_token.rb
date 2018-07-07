c75ba0e7cfb16e50eb133ab950db66f7493033ca
mdalmaruf <32615553+mdalmaruf@users.noreply.github.com>
2018-07-06 20:32:01 -0400

    Add files via upload

---
+++ Depot_reconstructed/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '451f3dab26894153fb568a8c454d976865263b187ee600e144fbb59b573577270978bd45c15790afaf4694966460e4ff18b9ea1766439bd82fc8e77f4af206de'
